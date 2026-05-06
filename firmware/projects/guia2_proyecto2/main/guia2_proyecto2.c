481cf14e3722337e345503985a4eb16e936e4758
Ljgonzalez7 <lucasjgonzalez25@gmail.com>
2026-05-06 12:41:14 -0300

    Actualizacion del proyecto2.

--- firmware/projects/guia2_proyecto2/main/guia2_proyecto2.c
+++ firmware/projects/guia2_proyecto2/main/guia2_proyecto2.c
@@ -2,7 +2,9 @@
  * Descripcion
  * @section genDesc General Description
  *
- 
+ * Edición del proyecto 1 adaptandolo al manejo de Interrupciones para reemplazo
+ * de las funciones con teclas y Timers.
+ *
  *
  *
  * @section hardConn Hardware Connection
@@ -45,12 +47,18 @@
 #include "gpio_mcu.h"
 #include "hc_sr04.h"
 #include "lcditse0803.h"
+#include "timer_mcu.h"
 
 /*==================[macros and definitions]=================================*/
 /** @def CONFIG_BLINK_PERIOD_LEDS 
  * @brief configuracion del periodo de parpadeo leds.
  */
 #define CONFIG_BLINK_PERIOD_LEDS 1000
+
+/** @def timer_tarea_1US
+ * @brief Variable que controla el timer de la tarea 1 (lectura de teclas) [us]
+ */
+#define timer_tarea_1US 1000000
 
 /** @def rangos 
  * @brief configuracion de umbrales de medición.
@@ -62,10 +70,12 @@
 
 
 /*==================[internal data definition]===============================*/
-/** @def leds_task_handle 
- * @brief Definicion que indica el orden de prioridad en el prosecamiento de tareas.
+/** @def TareaSensorDistancia_task_handle 
+ * @brief Definicion que indica el orden de prioridad en el procesamiento de tarea medir.
  */
-//TaskHandle_t led1_task_handle = NULL; PARA CUANDO USEMOS TAREAS
+
+TaskHandle_t TareaSensorDistancia_task_handle = NULL;
+
 /** @def medir 
  * @brief variable booleana filtro de medir o parar medición.
  */
@@ -105,30 +115,14 @@ void actualizarLeds(uint16_t distancia) {
 	
 }
 
-/** @fn void leerTeclas(bool *medir, bool *hold)
- * @brief Función que acciona según lectura de teclas.
- * * @param medir puntero a direccion de memoria de variable booleana medir.
- * * @param hold puntero a direccion de memoria de variable booleana hold.
- */
-
-void leerTeclas(bool *medir, bool *hold) {
-    uint8_t teclas = SwitchesRead();
-    if (teclas & SWITCH_1) {
-        *medir = !(*medir);
-        // vTaskDelay(100 / portTICK_PERIOD_MS); // Antirebote simple
-    }
-    if (teclas & SWITCH_2) {
-        *hold = !(*hold);
-    }
-}
 
 /** @fn void TareaSensorDistancia(void *pvParameter)
  * @brief Tarea que integra el programa de sensado, interacción y mostrado por pantalla.
  */
 
-void TareaSensorDistancia(void *pvParameter) {    
+static void TareaSensorDistancia(void *pvParameter) {    
 	while (1) {
-        leerTeclas(&medir, &hold); 
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);    /* La tarea espera en este punto hasta recibir una notificación */
 
         if (medir) {
             uint16_t distancia = HcSr04ReadDistanceInCentimeters();
@@ -145,10 +139,30 @@ void TareaSensorDistancia(void *pvParameter) {
 			LedOff(LED_3);
         }
 
-        vTaskDelay(CONFIG_BLINK_PERIOD_LEDS/ portTICK_PERIOD_MS);
     }
 }
 
+/**
+ * @brief Función invocada en la interrupción del timer, que envía una notificación a la tarea 1
+ *  para medir o no distancia, activar los leds o la pantalla LCD.
+ */
+void TimerInterrupcion(void* param){
+    vTaskNotifyGiveFromISR(TareaSensorDistancia_task_handle, pdFALSE);    /* Envía una notificación a la tarea 1 para interrumpirla */
+}
+
+/**
+ * @brief Función que atiende a la Tecla 1, que activa o desactiva la medición de distancia.
+ */
+void switch1_interrupcion (void* param){
+    medir =! medir;   /* Cambia el estado de medir */
+}
+
+/**
+ * @brief Función que atiende a la Tecla 2, que cambia el estado del hold (mantener o no el resultado).
+ */
+void switch2_interrupcion (void* param){
+    hold =! hold;                       /* Cambia el estado del hold */
+}
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
@@ -158,7 +172,25 @@ void app_main(void){
 	SwitchesInit();
 	HcSr04Init(GPIO_3, GPIO_2); 
 	
+
+    /* Inicialización de timers */
+    timer_config_t timer_1 = {
+        .timer = TIMER_A, 
+        .period = timer_tarea_1US, 
+        .func_p = TimerInterrupcion, 
+        .param_p = NULL
+    };
+
+    TimerInit(&timer_1);
+
 	printf("Ejecucion de programa\n");
-	xTaskCreate(&TareaSensorDistancia, "SensorDist", 2048, NULL, 5, NULL);
+	xTaskCreate(&TareaSensorDistancia, "SensorDist", 2048, NULL, 5, &TareaSensorDistancia_task_handle);
+    
+    /* Interrupciones por switch*/
+    SwitchActivInt(SWITCH_1, switch1_interrupcion, NULL);
+    SwitchActivInt(SWITCH_2, switch2_interrupcion, NULL);
+    
+    /* Inicialización del conteo de timers */
+    TimerStart(timer_1.timer);
 }
 /*==================[end of file]============================================*/
